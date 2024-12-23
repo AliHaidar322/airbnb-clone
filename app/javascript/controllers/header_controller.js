b54eb3799a2529dca02178212e7b17fbb71f06e5
AliHaidar322 <bcsf19a020@pucit.edu.pk>
2024-12-23 15:11:48 +0500

    Added stimulus contorller to toggle dropdown menu for user

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+import {enter, leave, toggle} from 'el-transition'
+
+export default class extends Controller {
+  static targets = ['openUserMenu', 'dropdown'];
+
+  connect( ){
+    this.openUserMenuTarget.addEventListener("click", (e) => {
+      toggle(this.dropdownTarget);
+    })
+  };
+}
