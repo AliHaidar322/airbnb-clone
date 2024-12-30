442891ab426e05b641efe71c45ecddadf8c4fd4c
AliHaidar322 <bcsf19a020@pucit.edu.pk>
2024-12-30 20:23:17 +0500

    Added images and settings for active storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Collect all image file paths in the `db/images/airbnb-images` directory
+image_files = Dir.glob('db/images/airbnb-images/*')
 
-20.times do
-  Property.create!({
+6.times do |i|
+  property = Property.create!({
     name: Faker::Lorem.unique.sentence(word_count: 3),
     description: Faker::Lorem.unique.paragraph(sentence_count: 2),
     headline:  Faker::Lorem.unique.sentence(word_count: 3),
@@ -20,4 +13,13 @@
     country: Faker::Address.unique.country,
     price: Money.from_amount((50...100).to_a.sample, 'USD'),
   })
+
+  # Attach two images to each property if available
+  if image_files[i]
+    property.images.attach(io: File.open(image_files[i]), filename: File.basename(image_files[i]))
+  end
+
+  if image_files[i + 6] # Use the next set of images
+    property.images.attach(io: File.open(image_files[i + 6]), filename: File.basename(image_files[i + 6]))
+  end
 end
