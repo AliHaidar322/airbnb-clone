0a8d0c4d6ce2d25e05bffac4c000685ed6995028
AliHaidar322 <bcsf19a020@pucit.edu.pk>
2024-12-30 18:39:42 +0500

    Added faker gem to create dummy data and used seed file to seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+20.times do
+  Property.create!({
+    name: Faker::Lorem.unique.sentence(word_count: 3),
+    description: Faker::Lorem.unique.paragraph(sentence_count: 2),
+    headline:  Faker::Lorem.unique.sentence(word_count: 3),
+    address_1: Faker::Address.unique.street_address,
+    address_2: Faker::Address.unique.secondary_address,
+    city: Faker::Address.unique.city,
+    state: Faker::Address.unique.state,
+    country: Faker::Address.unique.country,
+  })
+end
